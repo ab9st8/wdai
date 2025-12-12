8adf07e2f0069489fb6ccfaf950728ad3f5101ab
Antoni Chmiela <achmiela@student.agh.edu.pl>
2025-12-12 22:00:09 +0100

    use port 3000 internally in all containers, map to external ports

--- lab4/books/src/main.py
+++ lab4/books/src/main.py
@@ -9,9 +9,7 @@ async def ping():
 
 
 if __name__ == "__main__":
-	import os
 	import uvicorn
 
-	port = int(os.environ.get("PORT", "2137"))
-	uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=True)
+	uvicorn.run("src.main:app", host="0.0.0.0", port=3000, reload=True)
 
