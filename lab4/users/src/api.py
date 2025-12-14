e780ad7711b2dd82f64aa3aeb7264bee6f5996ee
Antoni Chmiela <achmiela@student.agh.edu.pl>
2025-12-14 18:36:45 +0100

    implement HEAD /api/users/{id}

--- lab4/users/src/api.py
+++ lab4/users/src/api.py
@@ -2,7 +2,7 @@ from pydantic import ValidationError
 from fastapi import Depends, Request, APIRouter, HTTPException, status, Response
 from model.base import db_session
 from model.domain.user import UserDTO
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, exists
 from model.orm.user import User
 
 router = APIRouter(prefix="/api")
@@ -22,6 +22,14 @@ async def get_users(db_session=Depends(db_session)):
     result = db_session.execute(query)
     return result.scalars().all()
 
+@router.head("/users/{user_id}", summary="Sprawdza, czy użytkownik o podanym id istnieje") # TIL
+async def get_user(user_id: int, db_session=Depends(db_session)):
+    query = select(exists().where(User.id == user_id))
+    result = db_session.execute(query).scalar()
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        
+    return Response(status_code=status.HTTP_200_OK)
 
 @router.post(
     "/users",
